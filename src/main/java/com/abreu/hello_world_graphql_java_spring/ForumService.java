810fce6de0ac756f228bc2e1053e95cbe8c62c6e
Rodrigo Abreu <rodrigodiasdeabreu@gmail.com>
2025-11-16 18:46:40 -0300

    feat: implementa um tratamento de erro

--- src/main/java/com/abreu/hello_world_graphql_java_spring/ForumService.java
+++ src/main/java/com/abreu/hello_world_graphql_java_spring/ForumService.java
@@ -4,42 +4,53 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-
 import org.springframework.stereotype.Service;
 
 @Service
 class PostService {
-    Map<String, Post> posts = new HashMap<>();
 
-    Collection<Post> createPost(final String content) {
-        var newPost = new Post(UUID.randomUUID().toString(), content);
-        posts.put(newPost.id(), newPost);
-        return posts.values();
-    }
-    
-    Post postById(String id) {
-        return posts.get(id);
-    }
+  Map<String, Post> posts = new HashMap<>();
 
-    void deletePost(String id) {
-        posts.remove(id);
-    }
-    
+  Collection<Post> createPost(final String content) {
+    var newPost = new Post(UUID.randomUUID().toString(), content);
+    posts.put(newPost.id(), newPost);
+    return posts.values();
+  }
+
+  Post postById(String id) {
+    return posts.get(id);
+  }
+
+  void deletePost(String id) {
+    posts.remove(id);
+  }
+
 }
+
 @Service
 class CommentService {
-        Map<String, Comment> comments = new HashMap<>();
 
-        Collection<Comment> createComment(final String content,final  String postId) {
-            var newComment = new Comment(UUID.randomUUID().toString(), content, postId);
-            comments.put(newComment.id(), newComment);
-            return comments.values();
-        }
+  final PostService postService;
 
-        public Collection<Comment> findByPostId(String postId) {
-            return comments.values().stream()
-                .filter(comment -> comment.postId().equals(postId))
-                .toList();
-        }
+  Map<String, Comment> comments = new HashMap<>();
+
+  public CommentService(PostService postService) {
+    this.postService = postService;
+  }
+
+  Collection<Comment> createComment(final String content, final String postId) {
+    var newComment = new Comment(UUID.randomUUID().toString(), content, postId);
+    comments.put(newComment.id(), newComment);
+    return comments.values();
+  }
+
+  public Collection<Comment> findByPostId(String postId) {
+    if (postService.postById(postId) == null) {
+      throw new NotFoundException("Post not found with id: " + postId);
+    }
+    return comments.values().stream()
+        .filter(comment -> comment.postId().equals(postId))
+        .toList();
+  }
 
 }
